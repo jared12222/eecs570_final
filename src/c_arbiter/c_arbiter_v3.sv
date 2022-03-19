9155d7ad35b36e215aad868fa2e3b148626a00e6
kyleku <kyleku1002@gmail.com>
2022-03-19 16:33:02 -0400

    c_arb const

--- src/c_arbiter/c_arbiter_v3.sv
+++ src/c_arbiter/c_arbiter_v3.sv
@@ -1,35 +1,32 @@
-`define	output_cnt 				4                      		//how many queues connected
-`define clause_width 			3 							//how many variables can be contain in the clause representation 
-`define element_cnt 			16 						//how many variables do the system support
-`define element_bit_cnt 		($clog2(`element_cnt) + 1)	//5
-`define	input_clause_cnt_bit	($clog2(`output_cnt) + 1)	//3 		
+`define element_bit_cnt 		($clog2(`LIT_INDEX_MAX) + 1)	//5
+`define	input_clause_cnt_bit	($clog2(`NUM_ENGINE) + 1)	//3 		
 
 
 module C_arbiter_v3(
 	input 																clock,
 	input 																reset,
-	input	[`output_cnt-1:0][`clause_width * `element_bit_cnt -1:0] 	clause_in,
+	input	[`NUM_ENGINE-1:0][`CLA_LENGTH * `element_bit_cnt -1:0] 		clause_in,
 	input	[`input_clause_cnt_bit-1:0]									clause_cnt_in,
-	input	[`output_cnt - 1:0] 										full_in,
+	input	[`NUM_ENGINE - 1:0] 										full_in,
 
-	output 	logic 	[`output_cnt-1:0][`clause_width * `element_bit_cnt -1:0] 	clause_out,
+	output 	logic 	[`NUM_ENGINE-1:0][`CLA_LENGTH * `element_bit_cnt -1:0] 		clause_out,
 	output 	logic 	[`input_clause_cnt_bit-1:0] 								clause_accept_out,
-	output 	logic 	[`output_cnt-1:0] 											grant_out
+	output 	logic 	[`NUM_ENGINE-1:0] 											grant_out
 );
 
 	logic 	[`input_clause_cnt_bit-1:0]	clause_cnt;
-	logic	[$clog2(`output_cnt)-1:0]	distribute_engine_idx, distribute_engine_idx_record;
+	logic	[$clog2(`NUM_ENGINE)-1:0]	distribute_engine_idx, distribute_engine_idx_record;
  
 	always_comb begin
 		clause_cnt 				= clause_cnt_in;
 		clause_accept_out 		= 0;
 		distribute_engine_idx 	= distribute_engine_idx_record;
-		for (int i = 0; i < `output_cnt; i++) begin
+		for (int i = 0; i < `NUM_ENGINE; i++) begin
 			clause_out[i] = 0;
 			grant_out[i]  = 0;
 		end
 
-		for (int i = 0; i < `output_cnt; i++) begin
+		for (int i = 0; i < `NUM_ENGINE; i++) begin
 			if (!clause_cnt) begin
 				break;
 			end
