483b5ced7f269a5957cc5db234788a38922d06a5
kyleku <kyleku1002@gmail.com>
2022-04-05 13:04:54 -0400

    style fixed

--- src/c_arbiter/L_buffer_singleload.sv
+++ src/c_arbiter/L_buffer_singleload.sv
@@ -22,10 +22,10 @@ module L_buffer_singleload (
 	input   lit_t                           uc_in,
 	input                                   load_uc_in,
 
-	output  cla_t   				        clause_out,
-	output 	logic[`NUM_ENGINE-1:0] 			clause_valid_out,
+	output  cla_t                           clause_out,
+	output 	logic[`NUM_ENGINE-1:0]          clause_valid_out,
 	output  ptr_t                           ptr_out, 
-	output 	logic							ptr_valid_out, uc_valid_out 		
+	output 	logic                           ptr_valid_out, uc_valid_out 		
 	//uc_out is placed in clause_out[0] since load uc and load clause won't happen at the same time
 );
 
@@ -33,7 +33,7 @@ module L_buffer_singleload (
 		logic	[$clog2(`NUM_ENGINE)-1:0]    engine_indicator, next_engine_indicator;
 	`endif
 
-	cla_t                           	 clause_in_blocked;
+	cla_t                                clause_in_blocked;
 	ptr_t                                ptr_in_blocked;
 	logic                                load_clause_in_blocked, load_ptr_in_blocked, load_change_engine_in_blocked;
 	lit_t                                uc_in_blocked;
@@ -62,7 +62,7 @@ module L_buffer_singleload (
 			`endif
 		end
 		if (load_ptr_in_blocked) begin
-			ptr_out                              = ptr_in_blocked;
+			ptr_out = ptr_in_blocked;
 			`ifndef ONE_ENGINE
 				ptr_valid_out[next_engine_indicator] = 1;
 			`else 
@@ -78,27 +78,27 @@ module L_buffer_singleload (
 	always_ff @(posedge clock)	begin
 		if (reset) begin
 			`ifndef ONE_ENGINE
-				engine_indicator 					<= 0;
+				engine_indicator             <= 0;
 			`endif
-			clause_in_blocked 					<= 0;
-			ptr_in_blocked  					<= 0;
-			load_clause_in_blocked 				<= 0; 
-			load_ptr_in_blocked 				<= 0;
-			load_change_engine_in_blocked  		<= 0;
-			uc_in_blocked 						<= 0;
-			load_uc_in_blocked 					<= 0;
+			clause_in_blocked                <= 0;
+			ptr_in_blocked                   <= 0;
+			load_clause_in_blocked           <= 0; 
+			load_ptr_in_blocked              <= 0;
+			load_change_engine_in_blocked    <= 0;
+			uc_in_blocked                    <= 0;
+			load_uc_in_blocked               <= 0;
 		end
 		else begin
 			`ifndef ONE_ENGINE
-			engine_indicator <= next_engine_indicator;
+			engine_indicator                 <= next_engine_indicator;
 			`endif
-			clause_in_blocked 					<= clause_in;
-			ptr_in_blocked  					<= ptr_in;
-			load_clause_in_blocked 				<= load_clause_in; 
-			load_ptr_in_blocked 				<= load_ptr_in;
-			load_change_engine_in_blocked  		<= load_change_engine_in;
-			uc_in_blocked 						<= uc_in;
-			load_uc_in_blocked 					<= load_uc_in;
+			clause_in_blocked                <= clause_in;
+			ptr_in_blocked                   <= ptr_in;
+			load_clause_in_blocked           <= load_clause_in; 
+			load_ptr_in_blocked              <= load_ptr_in;
+			load_change_engine_in_blocked    <= load_change_engine_in;
+			uc_in_blocked                    <= uc_in;
+			load_uc_in_blocked               <= load_uc_in;
 		end
 	end
 
