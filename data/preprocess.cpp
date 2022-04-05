7e2df57f56aa5ff2196dfccae51a7a4db32ba76f
Wei-Fan Tseng <wftseng@umich.edu>
2022-04-05 12:18:47 -0400

    modify preprocess to 2nd version. Add opt_analysis

--- data/preprocess.cpp
+++ data/preprocess.cpp
@@ -2,6 +2,9 @@
 #include <fstream> 
 #include <vector>   
 #include <stdlib.h>
+#include <algorithm>
+
+#define SORT
 
 using namespace std;
 
@@ -11,23 +14,25 @@ int num_of_clause;
 int padding;
 int num_clause_per_engine;
 int size_of_clause;
-int cal_padding(int num_of_engine, int num_of_clause){
-    int clause_per_engine = 1;
-    while(clause_per_engine * num_of_engine < num_of_clause){
-        clause_per_engine++;
-    }
+vector<vector<int> > clauses;
+vector<vector<vector<int> >> clauses_per_engine;
+// int cal_padding(int num_of_engine, int num_of_clause){
+//     int clause_per_engine = 1;
+//     while(clause_per_engine * num_of_engine < num_of_clause){
+//         clause_per_engine++;
+//     }
 
-    return clause_per_engine * num_of_engine - num_of_clause;
-}
+//     return clause_per_engine * num_of_engine - num_of_clause;
+// }
 
-int cal_clause_per_engine(int num_of_engine, int num_of_clause){
-    int clause_per_engine = 1;
-    while(clause_per_engine * num_of_engine < num_of_clause){
-        clause_per_engine++;
-    }
+// int cal_clause_per_engine(int num_of_engine, int num_of_clause){
+//     int clause_per_engine = 1;
+//     while(clause_per_engine * num_of_engine < num_of_clause){
+//         clause_per_engine++;
+//     }
 
-    return clause_per_engine;
-}
+//     return clause_per_engine;
+// }
 
 uint var(int literal) {
 	return abs(literal);
@@ -63,6 +68,28 @@ int cal_next_idx(vector<vector<int> > clauses, int start, int var){
 
 }
 
+bool compare (const vector<int> &clause_a, const vector<int> &clause_b)
+{
+    // cout<<"clause_a size = "<<clause_a.size()<<" clause_b size = "<<clause_b.size()<<endl; 
+//   for(int i=0; i<clause_a.size(); ++i){
+//     cout<<"i = "<<i<<endl;
+//     if(clause_a[i] < clause_b[i])
+//         return true;
+//   }
+//   return false;
+
+    // if(clause_a[0] < clause_b[0]){
+    //     cout<<"idx = 0"<<endl;
+    //     return true;
+    // }else if(clause_a[1] < clause_b[1]){
+    //     cout<<"idx = 1"<<endl;
+    //     return true;
+    // }else{
+    //     return false;
+    // }
+    return clause_a[0] < clause_b[0];
+}
+
 int main(int argc, char *argv[]){
 
     if(argc != 4){
@@ -95,96 +122,61 @@ int main(int argc, char *argv[]){
     string aux;
 	input_file >> aux >> num_of_var >> num_of_clause;
     // cout<<"num_of_engine = "<<num_of_engine<<" num_of_clause = "<<num_of_clause<<endl;
-    padding = cal_padding(num_of_engine, num_of_clause);
-    num_clause_per_engine = cal_clause_per_engine(num_of_engine, num_of_clause);
+    num_clause_per_engine = num_of_clause / num_of_engine; 
+    padding = num_of_clause - num_clause_per_engine * num_of_engine;
+
     // cout<<"after num_of_engine = "<<num_of_engine<<" num_of_clause = "<<num_of_clause<<endl;
     output_file << "Num_of_engine "<<num_of_engine<<endl;
-    output_file << "Num_of_clause_per_engine "<<num_clause_per_engine<<endl;
+    // output_file << "Num_of_clause_per_engine "<<num_clause_per_engine<<endl;
     output_file << "Num_of_padding "<<padding<<endl;
     output_file << "Num_of_var "<<num_of_var<<endl;
 
-    for(int i=0; i<num_of_engine - 1; i++){
-        // cout<<"engine = "<<i<<endl;
-        vector<vector<int> > clauses;
-        clauses.resize(num_clause_per_engine);
-        // vector<vector<vector<int>* > > positiveClauses;
-        // vector<vector<vector<int>* > > negativeClauses;
-        // positiveClauses.resize(num_of_var + 1);
-        // negativeClauses.resize(num_of_var + 1);
-        int literal;
-        for(int j=0; j<num_clause_per_engine; ++j){
-            while (input_file >> literal and literal != 0) {
-                clauses[j].push_back(literal);
+    clauses.resize(num_of_clause);
+    int literal;
+    // load all clauses into clauses vector
+    for(int i=0; i<num_of_clause; ++i){
+        while (input_file >> literal and literal != 0) {
+                clauses[i].push_back(literal);
             }
-            if(i==0 && j==0){
-                size_of_clause = clauses[0].size();
-                output_file << "Num_of_literal_per_clause "<<size_of_clause<<endl<<endl;
-            }
-        }
+    }
 
+    // sorting //
+    #ifdef SORT
+        cout<<"start sort"<<endl;
+        sort(clauses.begin(), clauses.end(), compare);
+        cout<<"end sort"<<endl;
+    #endif
+    // sorting //
+    output_file << "Num_of_lit_per_clause "<<clauses[0].size()<<endl<<endl;
+    clauses_per_engine.resize(num_of_engine);
+
+    for(int i=0; i<num_of_clause; ++i){
+        clauses_per_engine[i%num_of_engine].push_back(clauses[i]);
+    }
+    
+
+    for(int i=0; i<num_of_engine; i++){
         output_file << "Engine_num "<<i<<endl;
         output_file << "Head_node "<<endl;
         for (int k=1; k<num_of_var+1; ++k){
             output_file << k << " ";
-            output_file << cal_next_idx(clauses, -1, k) << endl;
+            output_file << cal_next_idx(clauses_per_engine[i], -1, k) << endl;
         }
         for (int k=1; k<num_of_var+1; ++k){
             output_file << -k << " ";
-            output_file << cal_next_idx(clauses, -1, -k) << endl;
+            output_file << cal_next_idx(clauses_per_engine[i], -1, -k) << endl;
         }
 
-        for(int j=0; j<num_clause_per_engine; ++j){
-            for(int k=0; k<clauses[j].size(); ++k){
-                output_file << clauses[j][k] << " ";
-                output_file << cal_next_idx(clauses, j+1, clauses[j][k])<< " ";
+        output_file << "Clause_node "<<clauses_per_engine[i].size()<<endl;
+        for(int j=0; j<clauses_per_engine[i].size(); ++j){
+            for(int k=0; k<clauses_per_engine[i][j].size(); ++k){
+                output_file << clauses_per_engine[i][j][k] << " ";
+                output_file << cal_next_idx(clauses_per_engine[i], j+1, clauses_per_engine[i][j][k])<< " ";
             }
             output_file<<endl;
         }
 
         output_file<<endl<<endl;
     }
-    
-    //last engine with padding
-    vector<vector<int> > clauses;
-    clauses.resize(num_clause_per_engine-padding);
-    
-    int literal;
-    for(int j=0; j<num_clause_per_engine-padding; ++j){
-        if(j<num_clause_per_engine-padding){
-            while (input_file >> literal and literal != 0) {
-                clauses[j].push_back(literal);
-            }
-        }
-    }
-
-    output_file << "Engine_num "<<num_of_engine-1<<endl;
-    output_file << "Head node "<<endl;
-    for (int k=1; k<num_of_var+1; ++k){
-        output_file << k << " ";
-        output_file << cal_next_idx(clauses, -1, k) << endl;
-    }
-    for (int k=1; k<num_of_var+1; ++k){
-        output_file << -k << " ";
-        output_file << cal_next_idx(clauses, -1, -k) << endl;
-    }
-
-    for(int j=0; j<num_clause_per_engine-padding; ++j){
-        for(int k=0; k<clauses[j].size(); ++k){
-            output_file << clauses[j][k] << " ";
-            output_file << cal_next_idx(clauses, j+1, clauses[j][k])<< " ";
-        }
-        output_file<<endl;
-    }
-
-    for(int j=0; j<padding; ++j){
-        for(int k=0; k<size_of_clause; ++k){
-            output_file << "0" << " ";
-            output_file << "-1" << " ";
-        }
-        output_file<<endl;
-    }
-
-
-    output_file<<endl<<endl;
 
 }
