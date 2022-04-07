818a2a059c6851c8a8a99dd74591d24be8a7450e
jared12222 <jared12222@gmail.com>
2022-04-07 15:30:55 -0400

    Update I/O

--- src/lookup/tb_top.sv
+++ src/lookup/tb_top.sv
@@ -5,9 +5,10 @@ module tb();
 logic       clk;
 logic       rst_n;
 logic       halt;
-node_t      carb2clq_node_in;
-logic       carb2clq_push;
-dummy_ptr_t carb2bcp_dummies;
+node_t      node_in;
+logic       node_in_valid;
+logic       change_eng;
+ptr_t       dummy_ptr;
 logic       carb2bcp_dummies_valid;
 logic       conflict;
 
@@ -15,15 +16,22 @@ top DUT(
     .clk(clk),
     .rst_n(rst_n),
     .halt(halt),
-    .carb2clq_node_in(carb2clq_node_in),
-    .carb2clq_push(carb2clq_push),
-    .carb2bcp_dummies(carb2bcp_dummies),
-    .carb2bcp_dummies_valid(carb2bcp_dummies_valid)
+    .node_in(node_in),
+    .node_in_valid(node_in_valid),
+    .change_eng(change_eng)    
+    .dummy_ptr(dummy_ptr),
+    .dummy_ptr_valid(dummy_ptr_valid),
+    .conflict(conflict)
 );
 
 always begin
     clk = ~clk;
     #(PERIOD/2);
+end
+
+initial begin
+    clk = 0;
+    rst_n = 1;
 end
 
 endmodule
