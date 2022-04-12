7234286df9ca6c5e2cc40c86cda46f44bf38feca
Wei-Fan Tseng <wftseng@umich.edu>
2022-04-12 01:08:29 -0400

    add data for all case

---
+++ data/gen_link_list.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+g++ -o preprocess preprocess.cpp -std=c++11
+echo -e "${BIWhite}Generating link list data...${Color_Off}"
+
+input_file_dir="/home/wftseng/li-sat-solver/sample_problems"
+output_bcp_trace="/home/wftseng/eecs570_final/data/preprocessed_data/"
+
+for i in 1 2 4 8 16
+do
+    for file in ${input_file_dir}/*.cnf
+    do
+        # ./sat.sh run $file $file".trace"
+        output_file="$(basename "${file}")"
+        output_file=$output_bcp_trace$i/"${output_file%.cnf}.linklist"
+        echo "Input file: "$file
+        echo "Output file: "$output_file
+        ./preprocess $i $file $output_file
+        # ./sat.sh compile 
+        # ./sat.sh run $file $output_file
+        # echo $file -ty
+    done
+done
